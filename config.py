095152fb3ce60d899fb4b625a22120ba3dce2670
hallma886 <hallma886@northwested.org>
2025-04-16 12:57:33 -0400

    Initial commit

---
+++ config.py
@@ -0,0 +1,67 @@
+# Pygame Configuration Module
+
+# Color contanants (RGB)
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+PURPLE = (255, 0, 255)
+ORANGE = (255, 95, 31)
+YELLOW = (255, 255, 0)
+FOREST_GREEN = (50, 140, 103)
+BROWN = (196, 164, 132)
+# Game window dimentions
+WINDOW_WIDTH = 900
+WINDOW_HEIGHT = 600
+
+# Window title (caption)
+# Update the window title as needed
+TITLE = "My Pygame Project"
+
+# Frame rate (frames per second)
+FPS = 60
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ main.py
@@ -0,0 +1,78 @@
+# Draw Lines in Pygame / No Functions
+
+# Pygame game template
+
+import pygame
+import sys
+import config  # Import the config module
+
+def init_game():
+    pygame.init()
+    screen = pygame.display.set_mode((config.WINDOW_WIDTH, config.WINDOW_HEIGHT))  # Use constants from config
+    pygame.display.set_caption(config.TITLE)
+    return screen
+
+def handle_events():
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            return False
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_ESCAPE:
+                return False  # Return False to indicate quitting
+    return True  # Continue running if no quit event
+
+def main():
+
+    screen = init_game()  # Initialize the game and get the screen
+    clock = pygame.time.Clock() # Initialize the clock objecct
+    
+
+ 
+    # Main game loop
+    running = True
+    while running:
+        running = handle_events()  # Handle events and check if we should continue running
+
+        # Fill the screen with a background color 
+        screen.fill(config.WHITE) 
+
+        pygame.display.flip()  # Update the display
+
+        clock.tick(config.FPS) # Limit frame rate to specified number of frames per second (FPS)
+
+    pygame.quit()  # Quit Pygame
+    sys.exit()  # Exit the program
+
+if __name__ == "__main__":
+    main()  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
